405d8fa38c052b0693e41667d13ff5b31f902e69
Alex <86182397+alehxs@users.noreply.github.com>
2025-10-25 21:34:46 -0500

    Initial Commit

---
+++ Menui/MenuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuiApp.swift
+//  Menui
+//
+//  Created by Alex on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
